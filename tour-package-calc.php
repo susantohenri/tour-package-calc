10edbe061f6086b5162d01c023de7d0a8101fdd2
Henri Susanto <henry.dinus@gmail.com>
2024-06-04 08:14:41 +0700

    done

--- tour-package-calc.php
+++ tour-package-calc.php
@@ -7,39 +7,50 @@
 * Author URI: http://github.com/susantohenri
 */
 
+define('TOUR_PACKAGE_CALC_DEFAULT_DROPDOWNS', [
+	[
+		'name' => 'package_selection',
+		'label' => 'PACKAGE SELECTION',
+		'options' => [
+			['text' => 'REFEREE', 'value' => 1199],
+			['text' => 'SOLO TRAVELER', 'value' => 1899],
+			['text' => 'COUPLE (WITH OPTION FOR KIDS)', 'value' => 1899]
+		]
+	],
+	[
+		'name' => 'extension',
+		'label' => 'EXTENSION',
+		'options' => [
+			['text' => 'FLIGHTS & 3 NIGHTS', 'value' => 0],
+			['text' => 'FLIGHTS & 5 NIGHTS', 'value' => 349],
+			['text' => 'FLIGHTS & 7 NIGHTS', 'value' => 698],
+			['text' => 'OWN FLIGHTS - 3 NIGHTS', 'value' => -700],
+			['text' => 'OWN FLIGHTS - 5 NIGHTS', 'value' => -351],
+			['text' => 'OWN FLIGHTS - 7 NIGHTS', 'value' => 2]
+		]
+	],
+	[
+		'name' => 'add_ons',
+		'label' => 'ADD ONS',
+		'options' => [
+			['text' => 'SINGLET', 'value' => 69],
+			['text' => 'CHILD (12 & UNDER)', 'value' => 899],
+			['text' => 'INFANT (24 MONTHS & UNDER)', 'value' => 199],
+		]
+	],
+]);
+
 add_shortcode('tour-package-calculator', function () {
-	$dropdowns = [
-		[
-			'name' => 'package_selection',
-			'label' => 'PACKAGE SELECTION',
-			'options' => [
-				['text' => 'REFEREE', 'value' => 1199],
-				['text' => 'SOLO TRAVELER', 'value' => 1899],
-				['text' => 'COUPLE (WITH OPTION FOR KIDS)', 'value' => 1899]
-			]
-		],
-		[
-			'name' => 'extension',
-			'label' => 'EXTENSION',
-			'options' => [
-				['text' => 'FLIGHTS & 3 NIGHTS', 'value' => 0],
-				['text' => 'FLIGHTS & 5 NIGHTS', 'value' => 349],
-				['text' => 'FLIGHTS & 7 NIGHTS', 'value' => 698],
-				['text' => 'OWN FLIGHTS - 3 NIGHTS', 'value' => -700],
-				['text' => 'OWN FLIGHTS - 5 NIGHTS', 'value' => -351],
-				['text' => 'OWN FLIGHTS - 7 NIGHTS', 'value' => 2]
-			]
-		],
-		[
-			'name' => 'add_ons',
-			'label' => 'ADD ONS',
-			'options' => [
-				['text' => 'SINGLET', 'value' => 69],
-				['text' => 'CHILD (12 & UNDER)', 'value' => 899],
-				['text' => 'INFANT (24 MONTHS & UNDER)', 'value' => 199],
-			]
-		],
-	];
+	$dropdowns = null;
+	if (!$dropdowns) $dropdowns = tour_package_calculator_option();
+	if (!$dropdowns) $dropdowns = tour_package_calculator_option(json_encode(TOUR_PACKAGE_CALC_DEFAULT_DROPDOWNS));
+	$dropdowns = json_decode($dropdowns);
+	$dropdowns = array_map(function ($select) {
+		$select->options = array_map(function ($options) {
+			return (array) $options;
+		}, $select->options);
+		return (array) $select;
+	}, $dropdowns);
 
 	$html = '';
 	foreach ($dropdowns as $d) {
@@ -107,3 +118,90 @@ add_shortcode('tour-package-calculator', function () {
 	";
 	return $html;
 });
+
+add_action('admin_menu', function () {
+	add_menu_page('Tour Package Calculator', 'Tour Package Calculator', 'manage_options', 'tour-package-calculator', function () {
+
+		$stored = null;
+		if (isset($_POST['tour_package_calculator_update_option'])) {
+			$stored = tour_package_calculator_option(str_replace('\"', '"', $_POST['tour_package_calculator_updated_option']));
+		} else if (isset($_POST['tour_package_calculator_reset_option'])) {
+			$stored = tour_package_calculator_option(json_encode(TOUR_PACKAGE_CALC_DEFAULT_DROPDOWNS));
+		}
+
+		if (!$stored) $stored = tour_package_calculator_option();
+		if (!$stored) $stored = tour_package_calculator_option(json_encode(TOUR_PACKAGE_CALC_DEFAULT_DROPDOWNS));
+
+		echo "
+			<br>
+			<br>
+			<form method=\"POST\" id=\"tour_package_calculator_form\">
+				<textarea style=\"display: none\" id=\"tour_package_calculator_updated_option\" name=\"tour_package_calculator_updated_option\" cols=\"100\" rows=\"5\">{$stored}</textarea>
+				<br>
+				<input id=\"tour_package_calculator_update_option\" name=\"tour_package_calculator_update_option\" type=\"submit\" class=\"button action\" value=\"Submit\">
+				<input name=\"tour_package_calculator_reset_option\" type=\"submit\" class=\"button action\" value=\"Reset\">
+			</form>
+			<script type=\"text/javascript\">
+				tour_package_calculator_to_inputs()
+				function tour_package_calculator_to_inputs () {
+					const form = jQuery(`#tour_package_calculator_form`)
+					const json = JSON.parse(jQuery(`#tour_package_calculator_updated_option`).val())
+					for (let select of json) {
+
+						const name = select.name
+						let placeholder = form.find(`#`+name)
+						if (1 > placeholder.length) {
+							jQuery(`<div class=\"tour_package_calculator_config_placeholder\" id=\"`+name+`\"><b>`+select.label+`</b></div><br>`).insertBefore(`#tour_package_calculator_update_option`)
+							placeholder = form.find(`#`+name)
+						} else placeholder.html(``)
+
+						for (let option of select.options) {
+							placeholder.append(`<br><span class=\"tour_package_calculator_config_option\">Text: <input class=\"text\" value=\"`+option.text+`\"> Price: <input class=\"value\" value=\"`+option.value+`\"></span>`)
+						}
+					}
+				}
+
+				function tour_package_calculator_to_json () {
+					const json = JSON.parse(jQuery(`#tour_package_calculator_updated_option`).val())
+					jQuery(`.tour_package_calculator_config_placeholder`).each(function() {
+						const name = jQuery(this).attr(`id`)
+						let options = []
+						jQuery(this).find(`.tour_package_calculator_config_option`).each(function () {
+							options.push({
+								text: jQuery(this).find(`.text`).val(),
+								value: jQuery(this).find(`.value`).val()
+							})
+						})
+						for (let i in json) {
+							if (json[i].name == name) json[i].options = options
+						}
+					})
+					jQuery(`#tour_package_calculator_updated_option`).val(JSON.stringify(json))
+					tour_package_calculator_to_inputs()
+				}
+				jQuery(`#tour_package_calculator_form`).submit(function () {
+					tour_package_calculator_to_json()
+					return true
+				})
+			</script>
+		";
+	});
+});
+
+function tour_package_calculator_option($option_value = null)
+{
+	global $wpdb;
+	$option_table = "{$wpdb->prefix}options";
+	$option_name = "tour-package-calculator-dropdowns";
+
+	$option = $wpdb->get_var("SELECT option_value FROM {$option_table} WHERE option_name = '{$option_name}'");
+
+	if (is_null($option_value)) return $option;
+	else if (!$option) $wpdb->insert($option_table, [
+		'option_name' => $option_name,
+		'option_value' => $option_value
+	]);
+	else $wpdb->update($option_table, ['option_value' => $option_value], ['option_name' => $option_name]);
+
+	return tour_package_calculator_option();
+}
